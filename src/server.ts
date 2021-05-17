62bf39f87a43142038a4ce5b3196f65a2d170db2
ck <klaudio.kumbula@gmail.com>
2021-05-17 14:07:35 +0200

    enviroment config

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express, { request, response } from 'express';
+//import './database';
+//import { router } from './routes';
+
+const app = express();
+const port = 8888;
+
+app.use(express.json());
+//app.use(router);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+}); 
